4c3dfd9e0666a22525bf038a261bef23c4d6090c
jill-gosrani <jillgosrani15@gmail.com>
2024-03-02 18:09:46 -0500

    Added button animations

--- game.js
+++ game.js
@@ -1,11 +1,31 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern =[];
 function nextSequence(){
     var randomChosenColour = buttonColours[Math.floor(Math.random() * 4)];
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
 
-    var audio = new Audio("./sounds/" + randomChosenColour + ".mp3");
+    
+}
+
+$(".btn").on("click",function (event){
+    var userChosenColour = event.target.id;
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    
+});
+
+function playSound(name){
+    var audio = new Audio("./sounds/" + name + ".mp3");
     audio.play();
+}
+function animatePress(currentColor){
+    $("#"+currentColor).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColor).removeClass('pressed');
+    }, 100);
 }
